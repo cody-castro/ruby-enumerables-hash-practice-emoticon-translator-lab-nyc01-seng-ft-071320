c951ceb82f3504213e5fc006a6979353fde231df
Cody C <codyicastro@gmail.com>
2020-07-02 23:23:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,56 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+ emoticons = YAML.load_file(file) 
+ hash_emo = {}
+  emoticons.each do |name,arrays|
+      hash_emo[name] ||= {}
+      hash_emo[name][:english] = arrays[0]
+      hash_emo[name][:japanese] = arrays[1]
+ end
+# binding.pry
+hash_emo
 end
 
-def get_japanese_emoticon
+# def get_english_meaning(file, emoticon)
+#   lib = load_library(file)
+#   translated = []
+#   lib.each do |meaning, lang|
+#     lang.map do |eng, jap|
+#       if jap == emoticon
+#   translated << meaning
+# end
+# end
+# end
+# else
+#   return "Sorry, that emoticon was not found"
+# end
+# end
+
+def get_english_meaning(file, emoticon)
+  lib = load_library(file)
+  lib.each do |meaning, lang|
+    return meaning if lang[:japanese] == emoticon
+  end
+    "Sorry, that emoticon was not found"
+ end 
+
+
+
+
+
+
+
+def get_japanese_emoticon(file_path, eng_emo)
   # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+    idioms.each do |eng, jap|
+      if eng == eng_emo 
+        return jap
+      end
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
-end
